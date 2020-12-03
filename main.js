a6056c855041ec3172c0eeea3d5409059013b013
Fotis Andritsos <fotisandritsos85@gmail.com>
2020-12-03 18:57:05 +0200

    code update

--- main.js
+++ main.js
@@ -1,62 +1,37 @@
 const tBlocks = document.querySelectorAll(".t-blocks");
+const xNiaou = "niaou";
+const oGav = "gav";
+const winPositions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 let xPlayer = true;
-let oPlayer = false;
 
-tBlocks.forEach((block) => {
-  block.addEventListener("click", (e) => {
-    blockTarg = e.target;
-    let indxTarger = [...blockTarg.parentElement.children].indexOf(blockTarg);
-
-    if (xPlayer) {
-      if (blockTarg.hasChildNodes()) {
-        return;
-      } else {
-        const act = document.createElement("div");
-        act.classList.add("niaou");
-        act.innerHTML = "X";
-        blockTarg.appendChild(act);
-        
-        xPlayer = false;
-        oPlayer = true;
-        checkWin(tBlocks, indxTarger);
-      }
-    } else {
-      if (blockTarg.hasChildNodes()) {
-        return;
-      } else {
-        const act = document.createElement("div");
-        act.classList.add("gav");
-        act.innerHTML = "O";
-        blockTarg.appendChild(act);
-        // checkWin(indxTarger);
-        xPlayer = true;
-        oPlayer = false;
-      }
-    }
-  });
-});
-const checkWin = (pos, inx) => {
-  const fook = pos[inx].children;
-  const xook = pos[inx + 1].children;
-  const oook = pos[inx - 1].children;
-  // console.log(fook);
-  // console.log(xook);
-  // console.log(oook);
-
-  let p;
-  for (p = 0; p < fook.length; p++) {
-    if (fook[p].classList.contains("niaou")) {
-      if (
-        xook[p].classList.contains("niaou") ||
-        oook[p].classList.contains("niaou")
-      ) {
-        console.log("WIINNNNN");
-      } else {
-        return;
-      }
-    } else {
-      return;
-    }
+const handleClick = (e) => {{
+    const cell = e.target;
+    const currentClass = xPlayer ? xNiaou : oGav;
+    placeMark(cell, currentClass);
+    tTurn();
   }
 };
+const tTurn = () => {
+  xPlayer = !xPlayer;
+};
+ 
+const placeMark = (cell, currentClass) =>{  
+  cell.classList.add(currentClass);
+}
+
+tBlocks.forEach((block) => {
+  block.addEventListener("click", handleClick, { once: true });
+});
+const getWinner = () => {
+
+}
